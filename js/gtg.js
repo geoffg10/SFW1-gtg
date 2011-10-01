696104a97570ff728366decef6e39a9ab2b26ac6
Geoffrey Ganga <gf4re@bellsouth.net>
2011-10-01 18:13:58 -0400

    Geoffrey Library

---
+++ js/gtg.js
@@ -0,0 +1,21 @@
+// GtG Library
+// Author: Geoffrey T. Ganga
+// Version 1.0 alpha
+
+
+var gtg = function(){
+	return new ryu.prototype.init();
+
+
+};
+
+gtg.prototype = {
+	
+	init:function(){
+		//init the library
+		
+	}
+
+};
+
+gtg.prototype.init.prototype = gtg.prototype;
